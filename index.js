e882a9676d210ec30646dfb9013c93745157fdce
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2022-06-26 09:42:53 +0530

    First

---
+++ index.js
@@ -0,0 +1,26 @@
+function vis(){
+    document.getElementById("explore").style.visibility="visible"
+}
+
+function hid()
+{
+    document.getElementById("explore").style.visibility="hidden"
+}
+
+function vis1(){
+    document.getElementById("experince").style.visibility="visible"
+}
+
+function hid1()
+{
+    document.getElementById("experince").style.visibility="hidden"
+}
+
+function vis2(){
+    document.getElementById("plan-your-trip").style.visibility="visible"
+}
+
+function hid2()
+{
+    document.getElementById("plan-your-trip").style.visibility="hidden"
+}
